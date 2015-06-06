eafb2458e5f5c2234f2fc96e4d63ac414c1db730
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-06-06 10:21:54 -0400

    Add Transitions

---
+++ The-Project/Assets/Scripts/Main.js
@@ -0,0 +1,15 @@
+/*  Main.js
+    Saturday, 6 June, 2015
+    
+    The Project
+    Alexander Rhett Crammer
+    Full Sail University  */
+
+$(document).ready(function () {
+  $('img[alt="Greeting.png"]').animate({
+    "top":"-95%"
+  }, 2000);
+  $('img[alt="MeetTheTeam.png"]').animate({
+    "top":"-85%"
+  }, 2000);
+});
